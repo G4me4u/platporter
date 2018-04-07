95425bb510aa34245beb0d3d7c303676c8ab8119
SirDancealot <mj@lpj-software.dk>
2018-04-07 14:47:48 +0200

    Added a door to complete the stage, no functionality yet

--- src/com/g4mesoft/platporter/world/tile/Tile.java
+++ src/com/g4mesoft/platporter/world/tile/Tile.java
@@ -24,6 +24,7 @@ public class Tile {
 	public static final Tile BEAM_STAND_TILE = new BeamStandTile();
 	public static final Tile BEAM_TILE = new BeamTile();
 	public static final Tile ORB_TILE = new OrbTile();
+	public static final Tile STAGE_CLEAR_TILE = new StageClearTile();
 	
 	private static final Map<Integer, Tile> ID_TO_TILE;
 	
@@ -67,6 +68,7 @@ public class Tile {
 		ID_TO_TILE.put(0x461F, BEAM_STAND_TILE);
 		ID_TO_TILE.put(0x434E, BEAM_TILE);
 		ID_TO_TILE.put(0x0026, ORB_TILE);
+		ID_TO_TILE.put(0x7F7F, STAGE_CLEAR_TILE);
 	}
 	
 	public static Tile parseTile(int id) {
