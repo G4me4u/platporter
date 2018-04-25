46e1ca45e0d92e73bf4552999bba91fd3ae12e34
G4me4u <G4me4u@gmail.com>
2018-04-25 12:39:03 +0200

    Simplified animation code

--- src/com/g4mesoft/platporter/world/tile/OrbTile.java
+++ src/com/g4mesoft/platporter/world/tile/OrbTile.java
@@ -9,6 +9,18 @@ public class OrbTile extends Tile {
 	private static final int TIME_MASK = 0x02 | 0x01;
 	
 	@Override
+	public void render(PPWorld world, Screen2D screen, int xt, int yt) {
+		int sy = 8;
+		
+		byte data = world.getData(xt, yt);
+		
+		int animTimer = ((int)(world.worldTime / 10L) + (data & TIME_MASK)) % 4;
+		int sx = (animTimer & 0x1) << ((animTimer & 0x2) >>> 1);
+		
+		screen.drawSprite(xt * 8, yt * 8, sx, sy, ColorPalette.getColors(050, 020, -1, -1));
+	}
+	
+	@Override
 	public boolean hasHitbox(PPWorld world, int xt, int yt) {
 		return false;
 	}
@@ -16,30 +28,5 @@ public class OrbTile extends Tile {
 	@Override
 	public boolean isBackgroundLayer(PPWorld world, int xt, int yt) {
 		return true;
-	}
-	
-	@Override
-	public void render(PPWorld world, Screen2D screen, int xt, int yt) {
-		int sy = 8;
-		
-		byte data = world.getData(xt, yt);
-		
-		int animTimer = ((int)(world.worldTime / 10L) + (data & TIME_MASK)) % 4;
-		
-		int sx = 0;
-		
-		if (animTimer % 2 != 0) {
-			if (animTimer % 3 != 0) {
-				sx = 1;
-			} else {
-				sx = 2;
-			}
-		} else {
-			sx = 0;
-		}
-		
-		
-		
-		screen.drawSprite(xt * 8, yt * 8, sx, sy, ColorPalette.getColors(050, 020, -1, -1));
 	}
 }
