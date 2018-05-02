585e8b9cd3a96dfe9a000debfb962e25c6bc3bcd
Sir Eraisuithon <mj@lpj-software.dk>
2018-05-02 16:30:19 +0200

    Added alternative art to Boost tile

--- src/com/g4mesoft/platporter/world/tile/BoostTile.java
+++ src/com/g4mesoft/platporter/world/tile/BoostTile.java
@@ -7,6 +7,8 @@ import com.g4mesoft.platporter.world.entity.PPEntity;
 
 public class BoostTile extends Tile {
 	
+	private static final int X_OFFSET_MASK = 0x01;
+	
 	@Override
 	public void steppedOn(PPWorld world, int xt, int yt, PPEntity entity) {
 		entity.velocity.y = -1.0f;
@@ -14,7 +16,10 @@ public class BoostTile extends Tile {
 	
 	@Override
 	public void render(PPWorld world, Screen2D screen, int xt, int yt) {
+		byte data = world.getData(xt, yt);
 		int sx = 4;
+		if ((data & X_OFFSET_MASK) != 0)
+			sx = 5;
 		int sy = 6;
 
 		screen.drawSprite(xt * 8, yt * 8, sx, sy, ColorPalette.getColors(541, 213, 123, 005));
