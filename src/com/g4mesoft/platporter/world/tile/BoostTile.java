6c4d59c831ba6a15b04bd29945f6f03234e31b73
SirDancealot <mj@lpj-software.dk>
2018-06-01 15:22:29 +0200

    Added boost tile to be toggled by lever and sprite variant to turned off version

--- src/com/g4mesoft/platporter/world/tile/BoostTile.java
+++ src/com/g4mesoft/platporter/world/tile/BoostTile.java
@@ -9,17 +9,33 @@ import com.g4mesoft.platporter.world.entity.PPEntity;
 public class BoostTile extends Tile {
 	
 	private static final int X_OFFSET_MASK = 0x01;
-	private static final int BOOST_GAIN_MASK = 0x0E;
+	private static final int BOOST_GAIN_MASK = 0x06;
+	private static final int OFF_MASK = 0x08;
+	private static final int ACTIVATE_ID_MASK = 0xF0;
 	
 	@Override
 	public void steppedOn(PPWorld world, int xt, int yt, PPEntity entity) {
 		if (!world.isClient())
 			return;
 		
-		float gain = ((world.getData(xt, yt) & BOOST_GAIN_MASK) >>> 1) * 0.5f + 1.0f;
-		entity.velocity.y = -0.71f * gain;
-		
-		Sounds.playSound(Sounds.BOOST_TILE_SOUND, 1.0f);
+		byte data = world.getData(xt, yt);
+		if ((data & OFF_MASK) == 0) {
+			float gain = ((world.getData(xt, yt) & BOOST_GAIN_MASK) >>> 1) * 0.5f + 1.0f;
+			entity.velocity.y = -0.71f * gain;
+			
+			Sounds.playSound(Sounds.BOOST_TILE_SOUND, 1.0f);
+		}
+	}
+	
+	@Override
+	public void toggleActivate(PPWorld world, int xt, int yt) {
+		byte data = world.getData(xt, yt);
+		world.setData(xt, yt, (byte)(data ^ OFF_MASK));
+	}
+	
+	@Override
+	public int getActivateId(PPWorld world, int xt, int yt) {
+		return (world.getData(xt, yt) & ACTIVATE_ID_MASK) >>> 4;
 	}
 	
 	@Override
@@ -29,6 +45,8 @@ public class BoostTile extends Tile {
 		if ((data & X_OFFSET_MASK) != 0)
 			sx = 5;
 		int sy = 6;
+		if ((data & OFF_MASK) != 0)
+			sy = 7;
 
 		screen.drawSprite(xt * 8, yt * 8, sx, sy, ColorPalette.getColors(541, 213, 123, 005));
 	}
